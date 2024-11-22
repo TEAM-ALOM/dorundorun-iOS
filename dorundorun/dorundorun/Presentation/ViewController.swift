4a2fe148f46a84712d0e82caca7cf6c5fcd7b499
SeoU <tjdnfkd1225@naver.com>
2024-11-22 20:58:49 +0900

    ✨ Feat: 프로젝트 생성
    - Main.storyboard 삭제
    - Info.plist 위치 변경에 따른 경로 수정

---
+++ dorundorun/dorundorun/Presentation/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  dorundorun
+//
+//  Created by eunseou on 11/22/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+        
+    }
+
+
+}
+
