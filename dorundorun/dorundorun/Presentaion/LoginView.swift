6cbfafb2dbd11a951e99982c8862ed94c4601a30
neaimt <pas120344@gmail.com>
2025-01-22 18:34:07 +0900

    Feat: 기본 색상 및 폰트 설정
    - 폰트 모디파이어 적용 예정

--- dorundorun/dorundorun/Presentaion/LoginView.swift
+++ dorundorun/dorundorun/Presentaion/LoginView.swift
@@ -26,15 +26,18 @@ struct LoginView: View {
         Text("안녕하세요!")
         Text("저 두런이와 함께 뛸 준비 되셨나요?")
       }
+      .font(.custom("Jalnan2", size: 20))
 
       Text("혼자 또는 같이 즐기는 비대면 러닝크루, 두런두런입니다.")
-        .foregroundStyle(Color.purple)
+        .foregroundStyle(Color.primary200)
+        .font(.custom("Pretendard-Regular", size: 13))
+        .padding(.top, 8)
       
       Spacer()
       
       createLoginButton(
         title: "카카오로 시작하기",
-        color: Color.yellow,
+        color: Color(red: 0.996, green: 0.898, blue: 0.0, opacity: 1.0),
         textColor: Color.black,
         iconName: "loginButtonKakao"
       ) {
