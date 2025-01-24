2dc6cd4548ce0037cec089d43eb926f99ce82cae
cinnamonfor6214 <seohyun2151@naver.com>
2025-01-24 18:34:18 +0900

    Fix : 탭바 UI 뷰로 할당
    피드백 반영하여 custom 탭바 UI로 만들었습니다!

---
+++ dorundorun/dorundorun/App/myPageView.swift
@@ -0,0 +1,19 @@
+//
+//  myPageView.swift
+//  dorundorun
+//
+//  Created by 박서현 on 1/24/25.
+//
+
+import SwiftUI
+
+struct myPageView: View {
+    var body: some View {
+        Text("마이페이지 화면")
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+#Preview {
+    myPageView()
+}
