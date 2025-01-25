517055900fd1ce08ec5ae5644b47c89a5dbb04bd
cinnamonfor6214 <seohyun2151@naver.com>
2025-01-25 12:01:15 +0900

    Fix: 코드 수정 및 Presentation으로 이동
    띄어쓰기, 줄바꿈 등 수정하고 모디파이어로 색상 수정했습니다!
    Presentation 디렉토리로 경로도 옮겼습니다.

---
+++ dorundorun/dorundorun/Presentaion/doodleRunView.swift
@@ -0,0 +1,19 @@
+//
+//  doodleRunView.swift
+//  dorundorun
+//
+//  Created by 박서현 on 1/24/25.
+//
+
+import SwiftUI
+
+struct doodleRunView: View {
+    var body: some View {
+        Text("두들런 화면")
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+#Preview {
+    doodleRunView()
+}
