b51565dbb9a7ac5f2d81a038a8e08e8c404e2a8e
SeoU <tjdnfkd1225@naver.com>
2025-01-26 19:48:53 +0900

    Docs: Create Workspace

---
+++ Workspace.swift
@@ -0,0 +1,19 @@
+//
+//  Workspace.swift
+//  ProjectDescriptionHelpers
+//
+//  Created by eunseou on 1/26/25.
+//
+
+import ProjectDescription
+
+let workspace = Workspace(
+    name: "Dorundorun",
+    projects: [
+        "Projects/Dorundorun",
+        "Projects/Presentation/**",
+        "Projects/Domain/**",
+        "Projects/Data/**"
+    ]
+)
+
