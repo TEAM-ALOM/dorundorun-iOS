b5a435f15dc7dfd9b2929bb9ad34e2ec26a20386
SeoU <tjdnfkd1225@naver.com>
2025-01-26 22:26:05 +0900

    Fix: 누락된 App 파일 추가

---
+++ Projects/Dorundorun/Sources/DorundorunApp.swift
@@ -0,0 +1,17 @@
+//
+//  DorundorunApp.swift
+//  
+//
+//  Created by eunseou on 1/26/25.
+//
+
+import SwiftUI
+
+@main
+struct MyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
