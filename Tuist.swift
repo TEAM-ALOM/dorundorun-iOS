e83d844ce4a62d14f53cf99dd5e8661bff045dda
SeoU <tjdnfkd1225@naver.com>
2025-01-26 19:49:09 +0900

    Docs: Create Tuist.swift

---
+++ Tuist.swift
@@ -0,0 +1,12 @@
+//
+//  Tuist.swift
+//  dorundorun-iOSManifests
+//
+//  Created by eunseou on 1/26/25.
+//
+
+import ProjectDescription
+
+let tuist = Tuist(
+    project: .tuist(generationOptions: .options(enforceExplicitDependencies: true))
+)
