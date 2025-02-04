d470ca6817b1a0b0c03424ef7a920b97fb7f5627
neaimt <pas120344@gmail.com>
2025-02-04 18:58:28 +0900

    Refactor : UI 레이아웃 개선 및 코드정리
    - VStack의 spacing값을 0으로 설정
    - 디자인에 맞는 패딩값 설정

--- Projects/Presentation/Login/Sources/LoginView.swift
+++ Projects/Presentation/Login/Sources/LoginView.swift
@@ -15,51 +15,51 @@ struct LoginView: View {
   }
   
   var body: some View {
-    VStack {
+    VStack(spacing: 0) {
       Spacer()
-        .frame(height: 383)
+        .frame(height: 329)
       
-      VStack(alignment: .leading) {
+      VStack(alignment: .leading, spacing: 0) {
         RoundedRectangle(cornerRadius: 5)
           .stroke(Color.gray)
           .fill(Color.nutral200)
           .frame(width: 102, height: 102)
+          .padding(.bottom, 16)
         
-        Group {
-          Text("안녕하세요!")
-          Text("두런이와 함께 뛰어요!")
-        }
-        .jalnan(.regular, size: 26)
-        .foregroundStyle(Color.primary200)
+        Text("안녕하세요!\n두런이와 함께 뛰어요!")
+          .frame(height: 70)
+          .lineSpacing(8)
+          .jalnan(.regular, size: 26)
+          .foregroundStyle(Color.primary200)
+          .padding(.bottom, 8)
         
         Text("혼자도, 함께도 즐기는 비대면 러닝크루")
           .suit(.medium, size: 15)
           .foregroundStyle(Color.nutral600)
-          .padding(.top, 2)
+          .padding(.bottom, 63)
       }
       .frame(maxWidth: .infinity, alignment: .leading)
       .padding(.leading, 16)
-      .padding(.bottom, 63)
       
-      
-      
-      createLoginButton(
-        title: "카카오로 시작하기",
-        color: Color(red: 0.996, green: 0.898, blue: 0.0, opacity: 1.0),
-        textColor: Color.black,
-        icon: DesignSystemAsset.Icons.kakao20
-      ) {
+      VStack(spacing: 8) {
+        createLoginButton(
+          title: "카카오로 시작하기",
+          color: Color(red: 0.996, green: 0.898, blue: 0.0, opacity: 1.0),
+          textColor: Color.black,
+          icon: DesignSystemAsset.Icons.kakao20
+        ) {
           // store로 action 전달 필요
         }
-      
-      createLoginButton(
-        title: "Apple로 시작하기",
-        color: Color.black,
-        textColor: Color.white,
-        icon: DesignSystemAsset.Icons.apple20
-      ) {
+        
+        createLoginButton(
+          title: "Apple로 시작하기",
+          color: Color.black,
+          textColor: Color.white,
+          icon: DesignSystemAsset.Icons.apple20
+        ) {
           // store로 action 전달 필요
         }
+      }
     }
   }
   
