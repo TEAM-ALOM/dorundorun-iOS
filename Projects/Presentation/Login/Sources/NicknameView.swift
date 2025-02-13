5e0b69c2875c4e9f4952ca6f94db6d2caefa73cc
neaimt <pas120344@gmail.com>
2025-02-14 08:45:40 +0900

    Feat: 문구 및 임시 아이콘 추가
    - 온보딩캐릭터 SVG, PNG 업로드 시도 했으나 실패함 ( 인식을 못함 )

--- Projects/Presentation/Login/Sources/NicknameView.swift
+++ Projects/Presentation/Login/Sources/NicknameView.swift
@@ -10,7 +10,21 @@ import DesignSystem
 
 struct NicknameView: View {
   var body: some View {
-    Text("Nickname")
+    VStack {
+      Text("안녕하세요!")
+        .padding(.bottom, 48)
+      
+      // 온보딩 캐릭터 파일 추가가 안됨 - 임시로 넣어둠
+      Rectangle()
+        .frame(width: 217, height: 217)
+        .foregroundStyle(Color.black)
+        .padding(.bottom, 72)
+      
+      Text("여러분과 함께 러닝을 하게 될")
+        .padding(.bottom, 24)
+    }
+    .jalnan(.regular, size: 18)
+    .foregroundStyle(Color.primary200)
   }
 }
 
