be99a4571f01c64bb250a0b3feacf27587c71634
neaimt <pas120344@gmail.com>
2025-02-14 07:15:54 +0900

    Feat: NicknameView 파일 생성

---
+++ Projects/Presentation/Login/Sources/NicknameView.swift
@@ -0,0 +1,15 @@
+//
+//  NicknameView.swift
+//  Login
+//
+//  Created by 박근경 on 2/14/25.
+//
+
+import SwiftUI
+import DesignSystem
+
+struct NicknameView: View {
+  var body: some View {
+    Text("Nickname")
+  }
+}
