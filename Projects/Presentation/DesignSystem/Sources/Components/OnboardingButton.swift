c2eaa1d03bf06534c322c128763461a055ca1ad8
neaimt <pas120344@gmail.com>
2025-02-14 07:59:58 +0900

    Feat: 버튼 version 추가
    - version 1인 경우 보라색
    - 그 외 회색

--- Projects/Presentation/DesignSystem/Sources/Components/OnboardingButton.swift
+++ Projects/Presentation/DesignSystem/Sources/Components/OnboardingButton.swift
@@ -10,28 +10,40 @@ import SwiftUI
 // MARK: 완료, 네 버튼
 struct OnboardingButton: View {
   
-  init(label: String) {
+  init(label: String, version: Int) {
     self.label = label
+    self.version = version
+  
+    if version == 1 {
+      self.fontColor = .nutralWhite
+      self.backColor = .primary200
+    } else {
+      self.fontColor = .nutral400
+      self.backColor = .nutral200
+    }
   }
   
   var body: some View {
     ZStack {
       RoundedRectangle(cornerRadius: 50)
-        .fill(Color.primary200)
+        .fill(backColor)
         .frame(width: .infinity, height: 48)
         .padding(.horizontal, 32)
       
       Text(label)
         .jalnan(.regular, size: 18)
-        .foregroundStyle(Color.nutralWhite)
+        .foregroundStyle(fontColor)
     }
   }
   
   private let label: String
+  private let version: Int
+  private var fontColor: Color
+  private var backColor: Color
 }
 
 #Preview {
-  OnboardingButton(label: "완료")
-  OnboardingButton(label: "네")
-  
+  OnboardingButton(label: "완료", version: 1)
+  OnboardingButton(label: "완료", version: 0)
+  OnboardingButton(label: "네", version: 1)
 }
