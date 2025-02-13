65642b981c39bce2805421c5e31b1002e77f3178
neaimt <pas120344@gmail.com>
2025-02-14 07:23:51 +0900

    Feat: OnboardingButton 컴포넌트 파일 생성
    - 파일 생성 및 프리뷰 추가

---
+++ Projects/Presentation/DesignSystem/Sources/Components/OnboardingButton.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingButton.swift
+//  DesignSystem
+//
+//  Created by 박근경 on 2/14/25.
+//
+
+import SwiftUI
+struct OnboardingButton: View {
+  var body: some View {
+    Text("완료")
+  }
+}
+
+#Preview {
+  OnboardingButton()
+}
