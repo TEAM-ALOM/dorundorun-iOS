0164b0348f8d10e11ce5d1e8ca3ea13c22c2b16f
neaimt <pas120344@gmail.com>
2025-02-14 09:04:35 +0900

    Feat: 완료 버튼 추가

--- Projects/Presentation/Login/Sources/NicknameView.swift
+++ Projects/Presentation/Login/Sources/NicknameView.swift
@@ -9,22 +9,30 @@ import SwiftUI
 import DesignSystem
 
 struct NicknameView: View {
+  @State var isNicknameCompleted: Bool = false
+  
   var body: some View {
     VStack {
-      Text("안녕하세요!")
-        .padding(.bottom, 48)
-      
-      // 온보딩 캐릭터 파일 추가가 안됨 - 임시로 넣어둠
-      Rectangle()
-        .frame(width: 217, height: 217)
-        .foregroundStyle(Color.black)
-        .padding(.bottom, 72)
-      
-      Text("여러분과 함께 러닝을 하게 될")
-        .padding(.bottom, 24)
+      Group {
+        Text("안녕하세요!")
+          .padding(.bottom, 48)
+        
+        // 온보딩 캐릭터 파일 추가가 안됨 - 임시로 넣어둠
+        Rectangle()
+          .frame(width: 217, height: 217)
+          .foregroundStyle(Color.black)
+          .padding(.bottom, 72)
+        
+        Text("여러분과 함께 러닝을 하게 될")
+          .padding(.bottom, 24)
+      }
+      .jalnan(.regular, size: 18)
+      .foregroundStyle(Color.primary200)
     }
-    .jalnan(.regular, size: 18)
-    .foregroundStyle(Color.primary200)
+    
+    isNicknameCompleted ?
+    OnboardingButton(label: "완료", version: 1) :
+    OnboardingButton(label: "완료", version: 0)
   }
 }
 
