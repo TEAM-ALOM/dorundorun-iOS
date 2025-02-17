ae0760af89aef9f7d1fa7f48827da814c0ebe880
neaimt <pas120344@gmail.com>
2025-02-17 21:33:44 +0900

    Feat: 키보드 위치에 따른 패딩 값 자동 변경

--- Projects/Presentation/Login/Sources/NicknameView.swift
+++ Projects/Presentation/Login/Sources/NicknameView.swift
@@ -19,16 +19,16 @@ struct NicknameView: View {
       
       Group {
         Text("안녕하세요!")
-          .padding(.bottom, 48)
+          .padding(.bottom, PaddingState.defaultState.firstTextPadding)
         
         // 온보딩 캐릭터 파일 추가가 안됨 - 임시로 넣어둠
         Rectangle()
           .frame(width: 217, height: 217)
           .foregroundStyle(Color.black)
-          .padding(.bottom, 72)
+          .padding(.bottom, PaddingState.defaultState.imagePadding)
         
         Text("여러분과 함께 러닝을 하게 될")
-          .padding(.bottom, 24)
+          .padding(.bottom, PaddingState.defaultState.secondTextPadding)
       }
       .jalnan(.regular, size: 18)
       .foregroundStyle(Color.primary200)
@@ -39,13 +39,13 @@ struct NicknameView: View {
         .stroke(Color.primary200, lineWidth: 1)
         .fill(Color.white)
         .frame(width: .infinity, height: 48)
-        .padding(.horizontal, 32)
       
       if ( !isKeyboardFocused ) {
         Text("사용할 닉네임을 입력해주세요.")
           .suit(.light, size: 15)
           .foregroundStyle(Color.nutral500)
       }
+      
       TextField("", text: $nickname)
         .jalnan(.regular, size: 18)
         .foregroundStyle(Color.nutralBlack)
@@ -58,6 +58,40 @@ struct NicknameView: View {
     isNicknameCompleted ?
     OnboardingButton(label: "완료", version: 1).padding(.bottom, 37) :
     OnboardingButton(label: "완료", version: 0).padding(.bottom, 37)
+  }
+  
+  private var padding: PaddingState { isKeyboardFocused ? .keyboardActive : .defaultState }
+      
+  enum PaddingState {
+    case defaultState
+    case keyboardActive
+      
+    var firstTextPadding: CGFloat {
+      switch self {
+      case .defaultState:
+        return 48
+      case .keyboardActive:
+        return 20
+      }
+    }
+    
+    var secondTextPadding: CGFloat {
+      switch self {
+      case .defaultState:
+        return 24
+      case .keyboardActive:
+        return 16
+      }
+    }
+    
+    var imagePadding: CGFloat {
+      switch self {
+      case .defaultState:
+        return 72
+      case .keyboardActive:
+        return 32
+      }
+    }
   }
 }
 
