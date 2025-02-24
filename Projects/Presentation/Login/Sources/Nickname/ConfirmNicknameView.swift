9d9d7648917f7428ad166342ae77bebf593a91f7
neaimt <pas120344@gmail.com>
2025-02-24 16:00:33 +0900

    Feat: CofirmNicknameView 파일 생성
    - 닉네임 최종 확정하는 화면

---
+++ Projects/Presentation/Login/Sources/Nickname/ConfirmNicknameView.swift
@@ -0,0 +1,19 @@
+//
+//  ConfirmNicknameView.swift
+//  Login
+//
+//  Created by 박근경 on 2/24/25.
+//
+
+import SwiftUI
+import DesignSystem
+
+struct ConfirmNicknameView: View {
+  var body: some View {
+    Text("Confirm Nickname")
+  }
+}
+
+#Preview {
+  ConfirmNicknameView()
+}
