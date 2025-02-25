13dd537b7b8d75e57d3262d49987f58c38b99944
neaimt <pas120344@gmail.com>
2025-02-25 14:39:31 +0900

    Feat: 닉네임, 안내 문구 추가

--- Projects/Presentation/Login/Sources/Nickname/ConfirmNicknameView.swift
+++ Projects/Presentation/Login/Sources/Nickname/ConfirmNicknameView.swift
@@ -10,7 +10,13 @@ import DesignSystem
 
 struct ConfirmNicknameView: View {
   var body: some View {
-    Text("Confirm Nickname")
+    Text("탱강이")
+      .jalnan(.regular, size: 18)
+      .foregroundStyle(Color.primary200)
+    
+    Text("사용가능한 닉네임이에요.\n이 닉네임으로 하시겠어요?")
+      .jalnan(.regular, size: 18)
+      .foregroundStyle(Color.primary200)
   }
 }
 
