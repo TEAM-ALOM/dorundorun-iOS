afca5cddcddfe6a56e2aed6f294f949d1e7e663b
neaimt <pas120344@gmail.com>
2025-03-03 00:27:16 +0900

    Feat: BackButton 파일 생성
    - 뒤로가기 버튼 컴포넌트 생성

---
+++ Projects/Presentation/DesignSystem/Sources/Components/BackButton.swift
@@ -0,0 +1,18 @@
+//
+//  BackButton.swift
+//  DesignSystem
+//
+//  Created by 박근경 on 3/3/25.
+//
+
+import SwiftUI
+
+struct BackButton: View {
+  var body: some View {
+    Text(">")
+  }
+}
+
+#Preview {
+  BackButton()
+}
