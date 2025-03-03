b6405f96689a7f94d279f09f59dfb2edb97f65ed
neaimt <pas120344@gmail.com>
2025-03-03 17:10:23 +0900

    Feat: 동일한 스타일의 텍스트를 함수로 구현

--- Projects/Presentation/Login/Sources/CharacterPants/CharacterPantsIntroView.swift
+++ Projects/Presentation/Login/Sources/CharacterPants/CharacterPantsIntroView.swift
@@ -11,6 +11,14 @@ import DesignSystem
 struct CharacterPantsIntroView: View {
   var body: some View {
     Text("CharacterPantsIntroView")
+
+// 동일한 속성의 텍스트를 함수로 사용
+private extension CharacterPantsIntroView {
+  func styledText(_ text: String, color: Color = Color.primary200) -> some View {
+    Text(text)
+      .jalnan(.regular, size: 18)
+      .foregroundStyle(color)
+      .multilineTextAlignment(.center)
   }
 }
 
