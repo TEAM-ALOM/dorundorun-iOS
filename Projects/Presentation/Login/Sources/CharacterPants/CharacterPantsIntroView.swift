40f1764cc606d74f733736869a412456aaa81e3e
neaimt <pas120344@gmail.com>
2025-03-03 17:06:55 +0900

    Feat: CharacterPantsIntroView 생성
    - 캐릭터 바지 선택 전 인트로 화면

---
+++ Projects/Presentation/Login/Sources/CharacterPants/CharacterPantsIntroView.swift
@@ -0,0 +1,19 @@
+//
+//  CharacterPantsIntroView.swift
+//  Login
+//
+//  Created by 박근경 on 3/3/25.
+//
+
+import SwiftUI
+import DesignSystem
+
+struct CharacterPantsIntroView: View {
+  var body: some View {
+    Text("CharacterPantsIntroView")
+  }
+}
+
+#Preview {
+  CharacterPantsIntroView()
+}
