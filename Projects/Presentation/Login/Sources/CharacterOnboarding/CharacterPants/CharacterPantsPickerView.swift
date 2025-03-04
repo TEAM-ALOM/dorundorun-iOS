624e4f89a1927a73c9fa59c6c77fac84a6558535
neaimt <pas120344@gmail.com>
2025-03-04 15:26:27 +0900

    Refactor: 컴포넌트 배치 변경

--- Projects/Presentation/Login/Sources/CharacterOnboarding/CharacterPants/CharacterPantsPickerView.swift
+++ Projects/Presentation/Login/Sources/CharacterOnboarding/CharacterPants/CharacterPantsPickerView.swift
@@ -10,15 +10,26 @@ import DesignSystem
 
 struct CharacterPantsPickerView: View {
   var body: some View {
-    Text("탱강이")
-      .jalnan(.regular, size: 18)
-      .foregroundStyle(Color.primary200)
+    VStack {
       BackButton()
         .padding(.bottom, 13)
+      
+      Text("탱강이")
+        .jalnan(.regular, size: 18)
+        .foregroundStyle(Color.primary200)
+        .padding(.bottom, 31)
+      
+      Image(asset: DesignSystemAsset.Icons.onboardingCharacter217)
+        .scaledToFit()
+        .frame(width: 211, height: 213)
+    }
+    .ignoresSafeArea()
     
-    Image(asset: DesignSystemAsset.Icons.onboardingCharacter217)
-      .scaledToFit()
-      .frame(width: 211, height: 213)
+    Spacer()
+      .frame(height: 48)
+    
+    Rectangle()
+      .frame(width: .infinity, height: 406)
   }
 }
 
