3552f004c3a36784e56f6dd6334c85c2e0a3052c
neaimt <pas120344@gmail.com>
2025-03-04 15:13:23 +0900

    Feat: CharacterPantsPickerView 파일 생성

---
+++ Projects/Presentation/Login/Sources/CharacterOnboarding/CharacterPants/CharacterPantsPickerView.swift
@@ -0,0 +1,19 @@
+//
+//  CharacterPantsPickerView.swift
+//  Login
+//
+//  Created by 박근경 on 3/4/25.
+//
+
+import SwiftUI
+import DesignSystem
+
+struct CharacterPantsPickerView: View {
+  var body: some View {
+    Text("CharacterPantsPickerView")
+  }
+}
+
+#Preview {
+  CharacterPantsPickerView()
+}
