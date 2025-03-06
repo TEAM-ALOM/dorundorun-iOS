8fcffbbd2d9dccbe01bead198ba947a44476b323
neaimt <pas120344@gmail.com>
2025-03-06 16:19:30 +0900

    Feat: CharacterBackgroundIntroVIew 파일 생성

---
+++ Projects/Presentation/Login/Sources/CharacterOnboarding/CharacterBackground/CharacterBackgroundIntroView.swift
@@ -0,0 +1,19 @@
+//
+//  CharacterBackgroundIntroVeiw.swift
+//  Login
+//
+//  Created by 박근경 on 3/6/25.
+//
+
+import SwiftUI
+import DesignSystem
+
+struct CharacterBackgroundIntroView: View {
+  var body: some View {
+    Text("CharaterBackgroundIntroView")
+  }
+}
+
+#Preview {
+  CharacterBackgroundIntroView()
+}
